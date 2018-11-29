b23fa4199df7b13c537bb206c892da9ecb256a56
Kevin Ndung'u <kevgathuku@gmail.com>
2018-11-29 16:53:46 +0300

    Add movie image atom

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Button } from "@storybook/react/demo";
+import MovieImage from "../../src/atoms/MovieImage";
 
 storiesOf("Button", module)
 	.add("with text", () => <Button>Hello Button</Button>)
@@ -11,3 +12,7 @@ storiesOf("Button", module)
 			</span>
 		</Button>
 	));
+
+storiesOf("MovieImage", module).add("with placeholder image", () => (
+	<MovieImage />
+));
---
+++ src/atoms/MovieImage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class MovieImage extends Component {
+	render() {
+		return (
+			<img
+				src="https://placeimg.com/200/290/nature"
+				alt="movie pic"
+				className="movie-image"
+			/>
+		);
+	}
+}
+
+export default MovieImage;
