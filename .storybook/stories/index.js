7f4ad54c195850a4928de9a8025bb4f8d86a2009
Kevin Ndung'u <kevgathuku@gmail.com>
2018-11-29 19:26:25 +0300

    Enable configuring the image source in MovieImage

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, text } from "@storybook/addon-knobs";
 import { Button } from "@storybook/react/demo";
 import MovieImage from "../../src/atoms/MovieImage";
 
 storiesOf("Button", module)
-	.add("with text", () => <Button>Hello Button</Button>)
-	.add("with some emoji", () => (
-		<Button>
-			<span role="img" aria-label="so cool">
-				😀 😎 👍 💯
-			</span>
-		</Button>
-	));
+  .add("with text", () => <Button>Hello Button</Button>)
+  .add("with some emoji", () => (
+    <Button>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
 
-storiesOf("MovieImage", module).add("with placeholder image", () => (
-	<MovieImage />
+const stories = storiesOf("MovieImage", module);
+
+// Add the `withKnobs` decorator to add knobs support to your stories.
+stories.addDecorator(withKnobs);
+
+stories.add("with placeholder image", () => (
+  <MovieImage src={text("Image URL", "https://placeimg.com/200/290/nature")} />
 ));
--- src/atoms/MovieImage.js
+++ src/atoms/MovieImage.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class MovieImage extends Component {
 	render() {
 		return (
 			<img
-				src="https://placeimg.com/200/290/nature"
+				src={this.props.src}
 				alt="movie pic"
 				className="movie-image"
 			/>
 		);
 	}
+}
+
+MovieImage.propTypes = {
+	src: PropTypes.string.isRequired
 }
 
 export default MovieImage;
